db077a70b54c033848a34a144078a32d9265b70c
Sanjarbek012 <sanjarbekxaytboyevv4@gmail.com>
2026-03-30 23:10:21 +0500

    commit

---
+++ sanjarbek.js
@@ -0,0 +1,91 @@
+// 1-masala 
+
+// Array.prototype.multiplyByTwo = function () {
+//   return this.map(num => num * 2);
+// };
+
+// console.log([1, 2, 3].multiplyByTwo()); 
+
+// 2-masala
+
+// String.prototype.reverseStr = function () {
+//   return this.split('').reverse().join('');
+// };
+
+// console.log("hello".reverseStr()); 
+
+// 3-masala
+// function User(name) {
+//   this.name = name;
+// }
+
+// User.prototype.sayHello = function () {
+//   return Hello ; $ ; {this.name};
+// };
+
+// const user1 = new User("Ali");
+// console.log(user1.sayHello())
+
+// 4-masala
+
+// function Car(name) {
+//   this.name = name;
+// }
+
+// Car.prototype.getName = function () {
+//   return this.name;
+// };
+
+// const car1 = new Car("BMW");
+// const car2 = new Car("Audi");
+
+// console.log([car1.getName(), car2.getName()]); 
+
+// 5-masala
+// Array.prototype.myConcat = function (arr) {
+//   return [...this, ...arr];
+// };
+
+// console.log([1, 2].myConcat([3, 4])); 
+
+// 6-masala
+// Object.prototype.myKeys = function () {
+//   return Object.keys(this);
+// };
+
+// const obj1 = { a: 1, b: 2 };
+// console.log(obj1.myKeys()); 
+
+// 7-masala
+// Object.prototype.sayHi = function () {
+//   return "Hi";
+// };
+
+// const obj2 = {};
+// console.log(obj2.sayHi()); 
+
+// delete Object.prototype.sayHi;
+
+// console.log("sayHi" in obj2); 
+
+// 8-masala
+// String.prototype.toUpperCustom = function () {
+//   return this.toUpperCase();
+// };
+
+// console.log("salom".toUpperCustom()); 
+
+// 9-masala
+// Array.prototype.sum = function () {
+//   return this.reduce((acc, val) => acc + val, 0);
+// };
+
+// console.log([1, 2, 3, 4].sum()); 
+
+// 10-masala
+// Object.prototype.valuesSum = function () {
+//   return Object.values(this).reduce((acc, val) => acc + val, 0);
+// };
+
+// const obj3 = { x: 10, y: 20, z: 5 };
+// console.log(obj3.valuesSum());
